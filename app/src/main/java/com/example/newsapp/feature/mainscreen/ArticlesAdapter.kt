3b228413b043c94f4c036a01160fe549a10a9574
king-of-the-frogs <vlad.osnos@mail.ru>
2023-03-11 16:15:23 +0300

    - визуал

---
+++ app/src/main/java/com/example/newsapp/feature/mainscreen/ArticlesAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.newsapp.feature.mainscreen
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsapp.R
+import com.example.newsapp.feature.domain.ArticleModel
+
+class ArticlesAdapter : RecyclerView.Adapter<ArticlesAdapter.ViewHolder>() {
+
+
+    private var articlesData: List<ArticlesAdapter> = emptyList()
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.tvTitle)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_article, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = articlesData[position].title
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = articlesData.size
+
+    fun setData (articles: List<ArticleModel>) {
+        articlesData = articles
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/com/example/newsapp/feature/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/example/newsapp/feature/mainscreen/MainScreenFragment.kt
@@ -1,28 +1,27 @@
 package com.example.newsapp.feature.mainscreen
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.R
-import com.example.newsapp.base.viewBinding
-import com.example.newsapp.databinding.FragmentMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainScreenFragment : Fragment(R.layout.fragment_main) {
 
-    private val binding by viewBinding { FragmentMainBinding.bind(it) }
     private val viewModel: MainScreenViewModel by viewModel()
+    private val recyclerView: RecyclerView by lazy { requireActivity().findViewById(R.id.rvArticles) }
+    private val adapter: ArticlesAdapter by lazy { ArticlesAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.viewState.observe(viewLifecycleOwner, ::render)
+        recyclerView.adapter = adapter
     }
 
-    @SuppressLint("SetTextI18n", "NewApi")
-    private fun render (viewState: ViewState){
-        binding.textView
-
+    private fun render (viewState: ViewState) {
+        adapter.setData(viewState.articles)
     }
 }
